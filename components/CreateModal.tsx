c495556a955e0e15172379780b830520293fd555
sabbir53rahman <sabbir53rahman@gmail.com>
2026-05-23 13:30:58 +0600

    complete file explorer

--- components/CreateModal.tsx
+++ components/CreateModal.tsx
@@ -43,7 +43,7 @@ export default function CreateModal({
           value={itemName}
           onChange={(e) => setItemName(e.target.value)}
           placeholder="Enter name..."
-          className="w-full px-3 py-2 border border-slate-300 rounded mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm"
+          className="w-full px-3 py-2 border border-slate-300 text-black rounded mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm"
           autoFocus
           onKeyDown={(e) => {
             if (e.key === "Enter") handleCreate();
